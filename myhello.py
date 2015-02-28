7d87c038b45564199b4b6d3e227e0b615d7c87af
vasalx <vasalexandr@gmail.com>
2015-02-28 03:06:33 +0200

    Switch to useing flask-wtf for forms management

--- myhello.py
+++ myhello.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'AVADAkEDAVRA'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(Form):
+    name = StringField('What is our name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -12,13 +24,21 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form,
+                            name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
     return  render_template('user.html', name = name)
+
+
 
 
 if __name__ == '__main__':
