730a6116bd778f279f874e10bb3c688b18a215ff
vasalx <vasalexandr@gmail.com>
2015-02-28 05:33:50 +0200

    Create DB table models

--- myhello.py
+++ myhello.py
@@ -21,9 +21,33 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 
+# database model definition
+class Role(db.Model):
+    # table definition
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+
+    # relationships
+    users = db.relationship('User', backref = 'role')
+
+    # class representation
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True, index = True)
+
+    # provide relationship
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class NameForm(Form):
-    name = StringField('What is our name?', validators=[Required()])
+    name = StringField('What is our name, my precious?', validators=[Required()])
     submit = SubmitField('Submit')
 
 @app.errorhandler(404)
